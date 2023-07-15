92fa00e7d72ae1220c88bc8d2b28681f4edcb441
Jess Bradshaw <134118247+jess-bradshaw@users.noreply.github.com>
2023-07-15 15:33:36 -0500

    Fixed materials & got an animal to change states on trigger

--- Assets/Scripts/TriggeredCollider.cs
+++ Assets/Scripts/TriggeredCollider.cs
@@ -7,15 +7,38 @@ public class TriggeredCollider : MonoBehaviour
 {
     public Material colourful; 
     public Material OGmaterial;
+    private bool friendNear = false; 
+    public Animator animator; 
     
     private void OnTriggerEnter(Collider other)
     {
-        GetComponent<MeshRenderer>().material = colourful; 
-        Debug.Log("collided"); 
+       if (this.gameObject.tag == "Animal")
+       { 
+       GetComponent<SkinnedMeshRenderer>().material = colourful;    
+       friendNear = true; 
+       animator.SetBool("friendNear", friendNear);
+      // Debug.Log("FRIEND"+ friendNear); 
+       }
+           
+       else 
+       {   
+            GetComponent<MeshRenderer>().material = colourful; 
+          //  Debug.Log("collided"); 
+       }
     }
       private void OnTriggerExit(Collider other)
     {
-        GetComponent<MeshRenderer>().material = OGmaterial; 
-        Debug.Log("exited"); 
+       if (this.gameObject.tag == "Animal")
+            { 
+            GetComponent<SkinnedMeshRenderer>().material = OGmaterial;
+            friendNear = false; 
+            animator.SetBool("friendNear", friendNear);
+           // Debug.Log("FRIEND gone"+ friendNear); 
+            }
+        else 
+        {
+             GetComponent<MeshRenderer>().material = OGmaterial; 
+            // Debug.Log("exited"); 
+        }
     }
 }
